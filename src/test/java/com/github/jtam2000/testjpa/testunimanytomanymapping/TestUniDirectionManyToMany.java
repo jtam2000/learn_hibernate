e7134af5927bbabc3d83bb17082dd79cc5c960a3
Jason Tam <jasontamemail@gmail.com>
2020-10-27 12:05:03 -0400

    TestUniDirectionManyToMany.java

---
+++ src/test/java/com/github/jtam2000/testjpa/testunimanytomanymapping/TestUniDirectionManyToMany.java
@@ -0,0 +1,31 @@
+package com.github.jtam2000.testjpa.testunimanytomanymapping;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestUniDirectionManyToMany {
+
+    @Before
+    public void runBeforeEachTest() {
+
+        createJPU();
+
+    }
+
+    private void createJPU() {
+
+        String JPUString = "jpu_relationship_many_to_many";
+
+    }
+
+    @Test
+    //CRud
+    public void test_createUnidirectional(){
+
+        //Given @Before:
+        //create a list of
+
+        //
+    }
+
+}
